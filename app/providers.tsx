5e7271a96d0e02aa76ba77faa2f3786a3d600fbe
rajeshgone <130182299+Rajeshgone@users.noreply.github.com>
2026-05-01 14:18:10 +0530

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "@/lib/wagmiConfig";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>;
+}
